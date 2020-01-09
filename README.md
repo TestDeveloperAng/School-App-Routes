ba46b563a32d0608651b469ffeccd40af1d74de1
TestDeveloperAng <rokdaking1024@gmail.com>
2020-01-09 13:19:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5m3clb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5m3clb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { HomeComponent } from './home/home.component';
+import { StudentsComponent } from './students/students.component';
+import { ClassroomsComponent } from './classrooms/classrooms.component';
+
+import { Routes,RouterModule } from '@angular/router';
+
+const appRoutes = [
+  {path : '', component : HomeComponent},
+  {path : 'students', component : StudentsComponent},
+  {path : 'classrooms', component : ClassroomsComponent},
+  {path : 'teachers', component : TeachersComponent},
+]
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, TeachersComponent, HomeComponent, StudentsComponent, ClassroomsComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
